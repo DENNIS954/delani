bccebe5d1f20085377d73e5fc7b45183762bc4f6
dennis Kiprono <denniskiprono954@gmail.com>
2020-02-19 07:56:21 -0500

    debugging

---
+++ Desktop/debugging/debugging/js/scripts.js
@@ -0,0 +1,6 @@
+function akanNames(){
+ let name ="John Doe";
+let nameArray = name.split(' '); 
+
+alert ("I have been called "+ nameArray[1] +"!!")  
+}
---
+++ Desktop/week3/static/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("img").show();
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+}); 
+
